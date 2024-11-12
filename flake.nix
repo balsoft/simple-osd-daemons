83b1bce3bc279deab1e40227d7f4ec5a5fb632a9
Alexander Bantyev <balsoft@balsoft.ru>
2024-11-12 11:12:13 +0400

    Fix pulseaudio & mpris

--- flake.nix
+++ flake.nix
@@ -12,64 +12,93 @@
 
   description = "A collection of simple on-screen-display daemons";
 
-  outputs = { self, nixpkgs, crate2nix }:
-    let forAllSystems = f: builtins.mapAttrs (_: f) nixpkgs.legacyPackages;
-    in {
-      packages = forAllSystems (pkgs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crate2nix,
+    }:
+    let
+      forAllSystems = f: builtins.mapAttrs (_: f) nixpkgs.legacyPackages;
+    in
+    {
+      packages = forAllSystems (
+        pkgs:
         let
-          commonDeps = with pkgs; [ libnotify gdk-pixbuf glib ];
+          commonDeps = with pkgs; [
+            libnotify
+            gdk-pixbuf
+            glib
+          ];
 
           inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
-            generatedCargoNix;
+            generatedCargoNix
+            ;
 
-          project = pkgs.callPackage (generatedCargoNix {
-            name = "simple-osd-daemons";
-            src = ./.;
-          }) {
-            defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-              simple-osd-battery = oa: { buildInputs = commonDeps; };
-              simple-osd-common = oa: {
-                buildInputs = commonDeps
-                  ++ [ pkgs.dbus.lib pkgs.dbus.dev ];
-                nativeBuildInputs = [ pkgs.pkg-config ];
+          project =
+            pkgs.callPackage
+              (generatedCargoNix {
+                name = "simple-osd-daemons";
+                src = ./.;
+              })
+              {
+                defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+                  simple-osd-battery = oa: { buildInputs = commonDeps; };
+                  simple-osd-common = oa: {
+                    buildInputs = commonDeps ++ [
+                      pkgs.dbus.lib
+                      pkgs.dbus.dev
+                    ];
+                    nativeBuildInputs = [ pkgs.pkg-config ];
+                  };
+                  simple-osd-brightness = oa: { buildInputs = commonDeps; };
+                  simple-osd-pulseaudio = oa: {
+                    buildInputs = commonDeps ++ [ pkgs.libpulseaudio ];
+                    postInstall = "patchelf --add-rpath ${pkgs.libpulseaudio}/lib $out/bin/*";
+                  };
+                  simple-osd-mpris = oa: {
+                    buildInputs = commonDeps ++ [ pkgs.libpulseaudio ];
+                    postInstall = "patchelf --add-rpath ${pkgs.libpulseaudio}/lib $out/bin/*";
+                  };
+                };
               };
-              simple-osd-brightness = oa: { buildInputs = commonDeps; };
-              # See https://github.com/kolloch/crate2nix/issues/149
-              libpulse-binding = oa: {
-                preBuild = "sed s/pulse::libpulse.so.0/pulse/ -i target/*link*";
-              };
-              simple-osd-pulseaudio = oa: {
-                buildInputs = commonDeps ++ [ pkgs.libpulseaudio ];
-                nativeBuildInputs = [ pkgs.pkg-config ];
-                preBuild = "sed s/pulse::libpulse.so.0/pulse/ -i target/*link*";
-              };
-              simple-osd-mpris = oa: {
-                buildInputs = commonDeps ++ [ pkgs.libpulseaudio ];
-                preBuild = "sed s/pulse::libpulse.so.0/pulse/ -i target/*link*";
-              };
-            };
-          };
-          membersList = builtins.attrValues (builtins.mapAttrs (name: member: {
-            name = pkgs.lib.removePrefix "simple-osd-" name;
-            value = member.build;
-          }) project.workspaceMembers);
-        in builtins.listToAttrs membersList);
-      apps = builtins.mapAttrs (_:
-        builtins.mapAttrs (_: pkg: {
-          type = "app";
-          program = "${pkg}/bin/${pkg.pname}";
-        })) self.packages;
+          membersList = builtins.attrValues (
+            builtins.mapAttrs (name: member: {
+              name = pkgs.lib.removePrefix "simple-osd-" name;
+              value = member.build;
+            }) project.workspaceMembers
+          );
+        in
+        builtins.listToAttrs membersList
+      );
+      apps = builtins.mapAttrs (
+        _:
+        builtins.mapAttrs (
+          _: pkg: {
+            type = "app";
+            program = "${pkg}/bin/${pkg.pname}";
+          }
+        )
+      ) self.packages;
 
-      defaultPackage = builtins.mapAttrs (system: pkgs:
+      defaultPackage = builtins.mapAttrs (
+        system: pkgs:
         pkgs.buildEnv {
           name = "simple-osd-daemons";
           paths = builtins.attrValues self.packages.${system};
-        }) nixpkgs.legacyPackages;
+        }
+      ) nixpkgs.legacyPackages;
 
-      devShell = builtins.mapAttrs (system: pkgs:
+      devShell = builtins.mapAttrs (
+        system: pkgs:
         pkgs.mkShell {
           inputsFrom = builtins.attrValues self.packages.${system};
-          buildInputs = [ pkgs.cargo pkgs.rust-analyzer pkgs.clippy ];
-        }) nixpkgs.legacyPackages;
+          buildInputs = [
+            pkgs.cargo
+            pkgs.rust-analyzer
+            pkgs.clippy
+          ];
+        }
+      ) nixpkgs.legacyPackages;
     };
 }
